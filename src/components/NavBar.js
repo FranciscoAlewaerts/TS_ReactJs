b1e9cf45adc9e2f55b0547cc8edd20ba42849d8b
FranciscoAlewaerts <93927312+FranciscoAlewaerts@users.noreply.github.com>
2022-02-26 16:42:33 -0300

    upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const NavBar = () => {
+    return (
+        <header>
+            <h1>TIENDA SPORTS</h1>
+            <nav>
+                    <ul>
+                        <li>
+                            <a href="#" className="txtNav">HOME</a>
+                        </li>
+                        <li>
+                            <a href="#" className="txtNav">CATEGORIAS</a>
+                        </li>
+                        <li>
+                            <a href="#" className="txtNav">CONTACTOS</a>
+                        </li>
+                    </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
