b96580be0b5916862d9b2fe700bec8a0da7a1c59
FranciscoAlewaerts <93927312+FranciscoAlewaerts@users.noreply.github.com>
2022-02-26 23:56:12 -0300

    upload

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main> 
+        <h1>Bienvenidos!</h1>
+        
+    </main>
+  )
+}
+
+export default Main
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,28 @@
 import React from "react"
+import CartWidget from "./CartWidget"
 
 const NavBar = () => {
     return (
         <header>
             <h1>TIENDA SPORTS</h1>
             <nav>
-                    <ul>
-                        <li>
-                            <a href="#" className="txtNav">HOME</a>
-                        </li>
-                        <li>
-                            <a href="#" className="txtNav">CATEGORIAS</a>
-                        </li>
-                        <li>
-                            <a href="#" className="txtNav">CONTACTOS</a>
-                        </li>
-                    </ul>
+                <ul>
+                    <li>
+                        <a href="#" className="txtNav">HOME</a>
+                    </li>
+                    <li>
+                        <a href="#" className="txtNav">CATEGORIAS</a>
+                    </li>
+                    <li>
+                        <a href="#" className="txtNav">CONTACTOS</a>
+                    </li>
+                    <li>
+                    < CartWidget />
+                    </li>
+                </ul>
+                
+                
+                
             </nav>
         </header>
     )
