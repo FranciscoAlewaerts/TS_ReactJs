00270a0de5f92172d190c14337d55ca6658b7f35
FranciscoAlewaerts <93927312+FranciscoAlewaerts@users.noreply.github.com>
2022-03-02 21:10:28 -0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import NavBar from "./components/NavBar"
+import ItemListContainer from "./components/ItemListContainer";
 
 function App() {
   return (
+    <>
+    
     <NavBar />
+    <ItemListContainer greeting={"hola"} />
+    
+    </>
   );
 }
 
